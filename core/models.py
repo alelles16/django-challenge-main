92aaf68dbc73fb480a55f0bd05f922952e1e0027
Ana Elles <alelles16@gmail.com>
2024-04-22 20:34:04 -0500

    feat: Add Docker and docker-compose configuration and change database

---
+++ core/models.py
@@ -0,0 +1,45 @@
+from django.db import models
+
+
+class Property(models.Model):
+    """
+        Model that represents a property.
+        A property could be a house, a flat, a hotel room, etc.
+    """
+    name = models.CharField(max_length=255, blank=True)
+    base_price = models.FloatField(null=True, blank=True)
+
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+
+class PricingRule(models.Model):
+    """
+        Model that represents a pricing rule that will be applied to a property when booking.
+        A rule can have a fixed price, or a percent modifier.
+        Only one rule can apply per day.
+        We can have multiple rules for the same day, but only the most relevant rule applies.
+    """
+    property = models.ForeignKey('core.Property', blank=False, null=False, on_delete=models.CASCADE)
+    price_modifier = models.FloatField(null=True, blank=True)
+    min_stay_length = models.IntegerField(null=True, blank=True)
+    fixed_price = models.FloatField(null=True, blank=True)
+    specific_day = models.DateField(null=True, blank=True)
+
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+
+class Booking(models.Model):
+    """
+        Model that represent a booking.
+        A booking is done when a customer books a property for a given range of days.
+        The booking model is also in charge of calculating the final price the customer will pay.
+    """
+    property = models.ForeignKey('core.Property', blank=False, null=False, on_delete=models.CASCADE)
+    date_start = models.DateField(blank=False, null=False)
+    date_end = models.DateField(blank=False, null=False)
+    final_price = models.FloatField(null=True, blank=True)
+
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
